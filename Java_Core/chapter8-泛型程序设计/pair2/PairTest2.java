fd5b61c16a9a971143d2eaf50e11035fa3492430
Waldenth <waldenth@outlook.com>
2021-03-19 19:14:30 +0800

    chpater 8 except 泛型和反射

--- Java_Core/chapter8-泛型程序设计/pair2/PairTest2.java
+++ Java_Core/chapter8-泛型程序设计/pair2/PairTest2.java
@@ -4,7 +4,17 @@ import pair1.Pair;
 import java.time.*; 
 
 public class PairTest2 {
-    
+    public static void main(String[]args){
+        LocalDate[]birthdays={
+            LocalDate.of(1906,12,9),
+            LocalDate.of(1815,12,10),
+            LocalDate.of(1903,12,3),
+            LocalDate.of(1910,6,22),
+        };
+        Pair<LocalDate> mm=ArrayAlg.minmax(birthdays);
+        System.out.println("min= "+mm.getFirst());
+        System.out.println("max= "+mm.getSecond());
+    }
 }
 
 class ArrayAlg{
@@ -14,7 +24,7 @@ class ArrayAlg{
         T min=a[0];
         T max=a[0];
         for(int i=1;i<a.length;i++){
-            if(min.compareTo(a[i]>0))//min-a[i]>0  min>a[i]
+            if(min.compareTo(a[i])>0)//min-a[i]>0  min>a[i]
                 min=a[i];
             if(max.compareTo(a[i])<0)//max<a[i]
                 max=a[i];
