1f53935b80812f741150896a16f28ca0fa8000a5
Waldenth <waldenth@outlook.com>
2021-03-29 22:44:30 +0800

    chapter 9 集合 part III

---
+++ Java_Core/chapter9-集合/sieve/Sieve.java
@@ -0,0 +1,35 @@
+package sieve;
+
+import java.util.BitSet;
+
+public class Sieve {
+    public static void main(String[]args){
+        int n=2000000;
+        long start=System.currentTimeMillis();
+        var bitSet=new BitSet(n+1);
+        int count=0;
+        int i;
+        for(i=2;i<=n;i++)
+            bitSet.set(i);
+        i=2;
+        while(i*i<=n){
+            if(bitSet.get(i)){
+                count++;
+                int k=2*i;
+                while(k<=n){
+                    bitSet.clear(k);
+                    k+=i;
+                }
+            }
+            i++;
+        }
+        while(i<=n){
+            if(bitSet.get(i))
+                count++;
+            i++;
+        }
+        long end=System.currentTimeMillis();
+        System.out.println(count+ " primes");
+        System.out.println((end-start)+" milliseconds");
+    }
+}
